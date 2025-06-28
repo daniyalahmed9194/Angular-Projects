6d3075a06a0ab3362bc16a013887c4e46e7993de
Abdur Rehman Daniyal <rehmanahm0987@gmail.com>
2025-06-28 18:39:59 +0500

    reveiw of forms

--- Working with Forms reactive_and_template_driven/src/app/auth/login-reactive/login-reactive.component.ts
+++ Working with Forms reactive_and_template_driven/src/app/auth/login-reactive/login-reactive.component.ts
@@ -14,8 +14,8 @@ import { debounceTime } from 'rxjs';
   templateUrl: './login-reactive.component.html',
   styleUrl: './login-reactive.component.css',
 })
-export class LoginReactiveComponent implements OnInit{
-  private destroyRef = inject(DestroyRef)
+export class LoginReactiveComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
   form = new FormGroup({
     email: new FormControl('', {
       validators: [Validators.email, Validators.required],
@@ -44,21 +44,25 @@ export class LoginReactiveComponent implements OnInit{
   ngOnInit(): void {
     const savedForm = window.localStorage.getItem('saved-login-form');
 
-    if(savedForm){
+    if (savedForm) {
       const loadFrom = JSON.parse(savedForm);
       this.form.patchValue({
-        email: loadFrom.email
-      })
+        email: loadFrom.email,
+      });
     }
 
+    const subscription = this.form.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe({
+        next: (value) => {
+          window.localStorage.setItem(
+            'saved-login-form',
+            JSON.stringify({ email: value.email })
+          );
+        },
+      });
 
-    const subscription = this.form.valueChanges.pipe(debounceTime(500)).subscribe({
-      next: (value) => {
-        window.localStorage.setItem('saved-login-form', JSON.stringify({email: value.email}))
-      }
-    })
-
-    this.destroyRef.onDestroy(() => subscription.unsubscribe)
+    this.destroyRef.onDestroy(() => subscription.unsubscribe);
   }
 
   onSubmit() {
